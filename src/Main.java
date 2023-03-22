55d67a8b77646a73133fcf29ed0f13f9cd13bdf3
Sara <saraeliza.lirio@gmail.com>
2023-03-22 15:19:10 -0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import desafio.dominio.Conteudo;
-import desafio.dominio.Curso;
-import desafio.dominio.Mentoria;
+import desafio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -11,19 +9,38 @@ public class Main {
         curso1.setDescricao("Curso Java Spring Boot");
         curso1.setCargaHoraria(2);
 
-        System.out.println(curso1);
-
         Mentoria mentoria1 = new Mentoria();
         mentoria1.setDescricao("Mentoria voltada a boas práticas SOLID");
         mentoria1.setTitulo("Solid");
         mentoria1.setData(LocalDate.now());
 
-        System.out.println(mentoria1);
-
         Conteudo conteudo = new Curso();
         conteudo.setTitulo("JavaScript");
         conteudo.setDescricao("Curso de JavaScript");
 
-        System.out.println(conteudo);
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Dio - Java Developer");
+        bootcamp.setDescricao("Voltado para estudos e prativa de Java");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(mentoria1);
+
+        Dev developer1 = new Dev();
+        developer1.setNome("Sara");
+        System.out.println(developer1.getNome());
+        developer1.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos: " + developer1.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos: " + developer1.getConteudosConcluidos());
+        System.out.println("XP: "+ developer1.calcularTotalXp());
+
+        System.out.println("--------------------------------");
+        Dev developer2 = new Dev();
+        developer2.setNome("João");
+        System.out.println(developer2.getNome());
+        developer2.inscreverBootcamp(bootcamp);
+        developer2.progredir();
+        developer2.progredir();
+        System.out.println("Conteúdos Inscritos: " + developer2.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos: " + developer2.getConteudosConcluidos());
+        System.out.println("XP: "+ developer2.calcularTotalXp());
     }
 }
